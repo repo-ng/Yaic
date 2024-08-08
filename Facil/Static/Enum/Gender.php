10be769f554362180ea44befd2cb819986b37301
AO™ <anthony@osawere.com>
2024-08-08 19:09:40 +0100

    Create Gender.php

---
+++ Facil/Static/Enum/Gender.php
@@ -0,0 +1,12 @@
+<?php //*** Enum ~ Gender » Yaic™ Library for Laravel © 2024 ∞ AO™ • @osawereao • www.osawere.com ∞ Apache License ***//
+
+namespace App\Yaic\Facil\Static\Enum;
+
+
+enum Gender: string
+{
+	// • case
+	case MALE = 'M';
+	case FEMALE = 'F';
+
+}//> end of Enum ~ Gender
