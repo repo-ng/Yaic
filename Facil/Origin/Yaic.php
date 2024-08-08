85427bbfacd17d4d39c2f6110fc256486de4959e
AO™ <anthony@osawere.com>
2024-08-08 18:59:44 +0100

    Create Yaic.php

---
+++ Facil/Origin/Yaic.php
@@ -0,0 +1,14 @@
+<?php //*** Yaic » Yaic™ Library for Laravel © 2024 ∞ AO™ • @osawereao • www.osawere.com ∞ Apache License ***//
+
+namespace App\Yaic\Facil\Origin;
+
+
+class Yaic
+{
+
+	// • constant
+	const NAME = 'Yet Another Inventive Code';
+	const ABBR = 'YAIC';
+	const BRAND = 'YAIC™';
+
+}//> end of Yaic
