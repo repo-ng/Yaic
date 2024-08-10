a253a6443c546ecfc5670956eb4e373c84856a25
AO™ <anthony@osawere.com>
2024-08-10 14:45:48 +0100

    Create TimeX.php

---
+++ Spry/Time/TimeX.php
@@ -0,0 +1,20 @@
+<?php //*** TimeX » Yaic™ Library for Laravel © 2024 ∞ AO™ • @osawereao • www.osawere.com ∞ Apache License ***//
+
+namespace App\Yaic\Spry\Time;
+
+
+class TimeX
+{
+
+	// • property
+
+
+
+
+
+	// • === method → ... » []
+	private static function method($var)
+	{
+	}
+
+}//> end of TimeX
