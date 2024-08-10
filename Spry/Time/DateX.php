306712194ba7815a7b41083b075f8e947775fc02
AO™ <anthony@osawere.com>
2024-08-10 14:45:43 +0100

    Create DateX.php

---
+++ Spry/Time/DateX.php
@@ -0,0 +1,20 @@
+<?php //*** DateX » Yaic™ Library for Laravel © 2024 ∞ AO™ • @osawereao • www.osawere.com ∞ Apache License ***//
+
+namespace App\Yaic\Spry\Time;
+
+
+class DateX
+{
+
+	// • property
+
+
+
+
+
+	// • === method → ... » []
+	private static function method($var)
+	{
+	}
+
+}//> end of DateX
